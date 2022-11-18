8eb8804c2f91ca281eedf1ac3a0fc0ffde63a2d6
Luzian Trapletti <luzian@trapletti.org>
2022-11-18 16:49:51 +0100

    add start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ps cax | grep gunicorn > /dev/null
+if [ $? -eq 0 ]; then
+  now=`date`
+  echo "$now - gunicorn is running"
+else
+  now=`date`
+  echo "$now - gunicorn is not running, starting"
+  cd /home/luzian/Textzeichen
+  source /home/luzian/miniconda3/etc/profile.d/conda.sh
+  conda activate main
+  gunicorn main:server -w 2 -b 127.0.0.1:8050 >& log.txt &
+fi
